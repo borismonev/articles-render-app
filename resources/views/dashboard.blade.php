d71ba5f34f959d935f46defc36ae4fd79f7705b9
Boris Monev <mone0002@hz.nl>
2025-06-06 01:12:55 +0200

    commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,121 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+<x-layout>
+    <h1 style="text-align: center">Dashboard</h1>
+    <hr>
+    <p class="subtitle is-5" style="text-align: center; width:80%; margin: auto auto 1.4rem auto">Here you can find
+        information about the
+        exams during the year and how many credits they give. You can also mark a course as completed and see how many
+        credits you have earned.</p>
+    <progress class="progress" value="15" max="60" style="width: 50%; position:relative; margin: auto"></progress>
+    <p style="text-align: center">Credits received: 10 / 60</p>
+    <br>
+    <table class="table is-narrow is-fullwidth is-bordered">
+        <tr> <!-- Table Header -->
+            <th> Block</th>
+            <th> Course</th>
+            <th> Exam</th>
+            <th> Study Credits
+            <th> Total</th>
+            <th> Grade Received</th>
+        </tr>
+        <!-- Block 1, Semester 1 -->
+        <!-- PCO -->
+        <tr>
+            <td rowspan="3"> B1/S1</td>
+            <td> PCO</td>
+            <td> Assesment Website</td>
+            <td> 2.5 EC</td>
+            <td rowspan="3"> 12.5 EC 20%</td>
+            <td> -</td>
+        </tr>
+        <!-- Computer Science Basics -->
+        <tr>
+            <td> Computer Science Basics</td>
+            <td> Written exam</td>
+            <td> 5 EC</td>
+            <td> -</td>
+        </tr>
+        <!-- Programming Basics -->
+        <tr>
+            <td> Programming Basics</td>
+            <td> Case study exam</td>
+            <td> 5 EC</td>
+            <td> -</td>
+        </tr>
+        <!-- B2 -->
+        <!-- Object-Oriented Programming -->
+        <tr>
+            <td rowspan="2"> B2/S1</td>
+            <td rowspan="2"> OOP</td>
+            <td> Group assignment</td>
+            <td> 5 EC</td>
+            <td rowspan="2"> 10 EC 17%</td>
+            <td rowspan="2"> -</td>
+        </tr>
+        <tr>
+            <td> Case study exam</td>
+            <td> 5 EC</td>
+        </tr>
+        <!-- Block 1, Semester 2-->
+        <!-- Framework project 1-->
+        <tr>
+            <td rowspan="4"> B1/S2</td>
+            <td rowspan="4"> Framework project 1</td>
+            <td> Written knowledge test</td>
+            <td> 5 EC</td>
+            <td rowspan="4"> 10 EC 17%</td>
+            <td rowspan="4"> -</td>
+        </tr>
+        <tr>
+            <td> Written knowledge test</td>
+            <td> 1.25 EC</td>
+        </tr>
+        <tr>
+            <td> Assignment (group)</td>
+            <td> 2.5 EC</td>
+        </tr>
+        <tr>
+            <td> Assignment (individual)</td>
+            <td> 1.25 EC</td>
+        </tr>
+        <tr>
+            <td rowspan="3"> B2/S2</td>
+            <td rowspan="3"> Framework Project 2</td>
+            <td> Final delivery</td>
+            <td> 2.5 EC</td>
+            <td rowspan="3"> 10 EC 17%</td>
+            <td rowspan="3"> -</td>
+        </tr>
+        <tr>
+            <td> Report of acceptance tests and optional assessments</td>
+            <td> 2.5 EC</td>
+        </tr>
+        <tr>
+            <td> IT Development portfolio</td>
+            <td> 5 EC</td>
+        </tr>
+        <tr>
+            <td> B1&2/S2</td>
+            <td> Business IT Consultancy basics</td>
+            <td> Video</td>
+            <td> 2.5 EC</td>
+            <td> 2.5 EC 4%</td>
+            <td> -</td>
+        </tr>
+        <tr>
+            <td> S1/S2</td>
+            <td> PPD</td>
+            <td> Criterium focused interview</td>
+            <td> 12.5 EC</td>
+            <td> 12.5 EC 20%</td>
+            <td> -</td>
+        </tr>
+        <tr>
+            <td> S1/S2</td>
+            <td> IT Personality</td>
+            <td> Portfolio</td>
+            <td> 2.5 EC</td>
+            <td> 2.5 EC 4%</td>
+            <td> -</td>
+        </tr>
+    </table>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 // Breeze / Laravel starter routes
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -23,21 +21,9 @@ Route::middleware('auth')->group(function () {
 
 // Your old routes — add them here
 Route::middleware('auth')->group(function () {
-    Route::get('', [TaskController::class, 'index'])->name('home');
-    Route::get('tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-    Route::post('', [TaskController::class, 'store'])->name('tasks.store');
-    //Route::post('{task}', [TaskController::class, 'toggleCompleted'])->name('tasks.toggleCompleted');
-    Route::get('tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-    Route::put('tasks/{task}/update', [TaskController::class, 'update'])->name('tasks.update');
-    Route::get('tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
-    Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-
-    Route::view('/about', 'about')->name('about');
-
-    Route::resource('posts', PostController::class);
-    Route::post('posts/{post}/comments', [PostController::class, 'storeComment'])->name('posts.storeComment');
-
-    Route::resource('projects', ProjectController::class);
+    foreach (ArticleController::getRoutes() as $postRoute) {
+        Route::{$postRoute['method']}($postRoute['uri'], [ArticleController::class, $postRoute['action']])->name($postRoute['name']);
+    }
 });
 
 // Load Breeze authentication routes (login, register, etc)
