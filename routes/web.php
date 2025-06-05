2a49309cc5f62cc636ce58590ca8f68e7e0e8671
Boris Monev <mone0002@hz.nl>
2025-06-06 01:18:23 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Breeze / Laravel starter routes
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -19,12 +14,13 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Your old routes — add them here
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
     foreach (ArticleController::getRoutes() as $postRoute) {
         Route::{$postRoute['method']}($postRoute['uri'], [ArticleController::class, $postRoute['action']])->name($postRoute['name']);
     }
 });
 
-// Load Breeze authentication routes (login, register, etc)
 require __DIR__ . '/auth.php';
